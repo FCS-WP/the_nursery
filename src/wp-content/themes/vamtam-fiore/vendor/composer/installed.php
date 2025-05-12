d7c883ce3b40ca691a2a6f4eb9cb4966816f6ee6
shin <nhathanh024@gmail.com>
2025-05-12 09:20:57 +0700

    init code

---
+++ src/wp-content/themes/vamtam-fiore/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cac3d0608ccbb4c93e9e8f52b75086ecfcf27b82',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cac3d0608ccbb4c93e9e8f52b75086ecfcf27b82',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
