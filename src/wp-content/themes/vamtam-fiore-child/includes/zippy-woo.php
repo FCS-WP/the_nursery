da9513a3885eedcab9606d1a41f9d88c669641c8
toanpham2111 <toan444666@gmail.com>
2025-06-13 14:31:32 +0700

    update

--- src/wp-content/themes/vamtam-fiore-child/includes/zippy-woo.php
+++ src/wp-content/themes/vamtam-fiore-child/includes/zippy-woo.php
@@ -240,3 +240,20 @@ function plant_combo_shortcode($atts)
 <?php
     return ob_get_clean();
 }
+
+
+function exclude_workshop_products_from_archive( $query ) {
+    if ( ! is_admin() && $query->is_main_query() && ( is_shop() || is_product_category() || is_product_tag() || is_search() ) ) {
+        $tax_query = (array) $query->get( 'tax_query' );
+
+        $tax_query[] = array(
+            'taxonomy' => 'product_cat',
+            'field'    => 'slug',
+            'terms'    => array( 'workshop' ),
+            'operator' => 'NOT IN',
+        );
+
+        $query->set( 'tax_query', $tax_query );
+    }
+}
+add_action( 'pre_get_posts', 'exclude_workshop_products_from_archive' );
